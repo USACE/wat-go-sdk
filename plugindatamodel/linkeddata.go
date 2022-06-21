0b68c3af0a0eddbaa01045e70aa9d919e68a87cf
Will Lehman <william.p.lehman@usace.army.mil>
2022-06-21 12:25:53 -0400

    updating the text in datamodel.go to reflect the latest datamodel

--- plugindatamodel/linkeddata.go
+++ plugindatamodel/linkeddata.go
@@ -5,6 +5,7 @@ type LinkedData struct {
 	//Data
 	Data `json:"data" yaml:"data"`
 	//Provider a provider is a specific output data element from a manifest.
-	SourceData Data          `json:"source_data" yaml:"source_data"`
-	Provider   ModelManifest `json:"provider" yaml:"provider"` //could just be modelidentifier and plugin i believe.
+	SourceData Data `json:"source_data" yaml:"source_data"`
+
+	Provider ModelManifest `json:"provider" yaml:"provider"` //could just be modelidentifier and plugin i believe.
 }
