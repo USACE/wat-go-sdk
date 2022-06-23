3bad1407ae6b37e53170d029885df53bc33393e1
Will Lehman <william.p.lehman@usace.army.mil>
2022-06-23 11:39:47 +0000

    adding comments and removing commented out structs

--- plugindatamodel/data.go
+++ plugindatamodel/data.go
@@ -1,8 +1,10 @@
 package plugindatamodel
 
 type Data struct {
+	//FileName describes the name of the file that needs to be input or output.
 	FileName string `json:"filename" yaml:"filename"`
-	Path     string `json:"path,omitempty" yaml:"path,omitempty"`
+	//Path describes the specific information in the file (e.g. /a/b/c/d/e/f for dss)
+	Path string `json:"path,omitempty" yaml:"path,omitempty"`
 }
 
 /*
