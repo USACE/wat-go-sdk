866b9fdf7d88adbbb83474d318eff9acdb6cf287
Will Lehman <william.p.lehman@usace.army.mil>
2022-06-23 13:36:43 +0000

    adding in updates for manifest, linked manifest, and payload

--- plugindatamodel/data.go
+++ plugindatamodel/data.go
@@ -7,33 +7,5 @@ type Data struct {
 	Path string `json:"path,omitempty" yaml:"path,omitempty"`
 }
 
-/*
-type DataContainer struct {
-	Type        string `json:"type" yaml:"type"`
-	DataElement `json:"data" yaml:"data"`
-}
-type DataElement interface {
-	DataElementType() string
-}
-
-//FileData
-type File struct {
-	//FileName
-	FileName string `json:"filename" yaml:"filename"`
-}
-
-func (fd File) DataElementType() string {
-	return strings.ToLower(reflect.TypeOf(fd).Name())
-}
-
-//FileAndPathData
-type FileAndPath struct {
-	Type string `json:"type" yaml:"type"`
-	//FileName
-	FileName string `json:"filename" yaml:"filename"`
-	//Path describes the specific information in the file (e.g. /a/b/c/d/e/f for dss)
-	Path string `json:"path" yaml:"path"`
-}
-*/
 //acceptable formats? format options?
 //optional/required
