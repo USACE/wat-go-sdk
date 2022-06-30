ea63d49cde2f8624ef222e3a7de0943cd414836a
Will Lehman <william.p.lehman@usace.army.mil>
2022-06-30 11:31:41 +0000

    testing out uuids

--- main.go
+++ main.go
@@ -12,8 +12,18 @@ func main() {
 	//uuid.SetNodeID([]byte{1, 2, 3, 4, 5, 6})
 	//uuid.Variant
 	//fmt.Println(uuid.NodeID())
-
-	for i := 1; i < 40; i++ {
+	manifestID := uuid.New()
+	fmt.Println(manifestID)
+	file1 := uuid.NewSHA1(manifestID, []byte("Input-Muncie.tmp.hdf"))
+	file2 := uuid.NewSHA1(manifestID, []byte("Output-Muncie.tmp.hdf"))
+	fmt.Println(file1)
+	fmt.Println(file2)
+	parsed1 := uuid.MustParse(file1.String())
+	parsed2 := uuid.MustParse(file2.String())
+	fmt.Println(parsed1.Value())
+	fmt.Println(parsed2.Domain().String())
+	/*for i := 1; i < 40; i++ {
 		fmt.Println(fmt.Sprintf("%v,%v", i, uuid.New()))
 	}
+	*/
 }
