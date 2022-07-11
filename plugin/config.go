deeaac9e6e9f7ad6d1144857c7001736607537da
Will Lehman <william.p.lehman@usace.army.mil>
2022-07-11 18:00:07 +0000

    marking optional fields in the awsconfig

--- plugin/config.go
+++ plugin/config.go
@@ -3,16 +3,16 @@ package plugin
 import "errors"
 
 type AwsConfig struct {
-	Name                  string `json:"aws_config_name"`
+	Name                  string `json:"aws_config_name,omitempty"`
 	IsPrimary             bool   `json:"is_primary_config"` //where payloads would get stored?
 	AWS_ACCESS_KEY_ID     string `json:"aws_access_key_id"`
 	AWS_SECRET_ACCESS_KEY string `json:"aws_secret_access_key_id"`
 	AWS_REGION            string `json:"aws_region"`
 	AWS_BUCKET            string `json:"aws_bucket"`
-	S3_MOCK               bool   `json:"aws_mock"`             //for testing with minio
-	S3_ENDPOINT           string `json:"aws_endpoint"`         //for testing with minio
-	S3_DISABLE_SSL        bool   `json:"aws_disable_ssl"`      //for testing with minio
-	S3_FORCE_PATH_STYLE   bool   `json:"aws_force_path_style"` //for testing with minio
+	S3_MOCK               bool   `json:"aws_mock,omitempty"`             //for testing with minio
+	S3_ENDPOINT           string `json:"aws_endpoint,omitempty"`         //for testing with minio
+	S3_DISABLE_SSL        bool   `json:"aws_disable_ssl,omitempty"`      //for testing with minio
+	S3_FORCE_PATH_STYLE   bool   `json:"aws_force_path_style,omitempty"` //for testing with minio
 }
 type Config struct {
 	AwsConfigs []AwsConfig `json:"aws_configs"`
